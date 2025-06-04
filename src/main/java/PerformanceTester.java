6d376580a0a8015675a1d2736deabf9b801227e6
7vanael <kristi.n.norris777@gmail.com>
2025-06-04 08:43:04 -0700

    QuickSort performance test runs as expected, refactored main, added PerformanceTester Class

---
+++ src/main/java/PerformanceTester.java
@@ -0,0 +1,164 @@
+import java.util.Random;
+
+public class PerformanceTester {
+
+    private final int SIZE_OF_SORTED_DATA_SET = 10000;
+    private final int SIZE_OF_ADDED_DATA_SET = 1000;
+
+    private Random random = new Random();
+    private int[] tenThousand = new int[SIZE_OF_SORTED_DATA_SET];
+
+    private long startTime;
+    private long endTime;
+    private long duration;
+
+    public void runPerformanceTest(){
+        System.out.println("ArrayList Performance Testing");
+        System.out.println("=============================\n");
+
+        addToFrontArrayList();
+        addToEndArrayList();
+        addToMiddleArrayList();
+
+        System.out.println("LinkedList Performance Testing");
+        System.out.println("=============================\n");
+
+        addToFrontLinkedList();
+        addToEndLinkedList();
+        addToMiddleLinkedList();
+
+
+        System.out.println("QuickSort Performance Testing");
+        System.out.println("=============================\n");
+
+
+        quickSortPerformance();
+    }
+
+    private void quickSortPerformance() {
+        ArrayList array;
+        LinkedList linked;
+
+        populateTenThousandRandom(tenThousand, random);
+
+        array = new ArrayList();
+        linked = new LinkedList();
+
+        for (int i = 0; i < SIZE_OF_SORTED_DATA_SET; i++) {
+            array.add(tenThousand[i]);
+            linked.add(tenThousand[i]);
+        }
+
+        startTime = System.nanoTime();
+        QuickSort.quickSort(linked);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("QuickSorting LinkedList:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + linked.size() + "\n");
+
+        startTime = System.nanoTime();
+        QuickSort.quickSort(array);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("QuickSorting ArrayList:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + array.size() + "\n");
+    }
+
+    private void populateTenThousandRandom(int[] tenThousand, Random random) {
+        for (int i = 0; i < SIZE_OF_SORTED_DATA_SET; i++) {
+            tenThousand[i] = random.nextInt();
+        }
+    }
+
+    public void addToFrontArrayList() {
+        ArrayList array = new ArrayList();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < SIZE_OF_ADDED_DATA_SET; i++) array.add(i, 0);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("Adding to Front:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + array.size() + "\n");
+    }
+
+    public void addToEndArrayList() {
+        ArrayList array = new ArrayList();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < SIZE_OF_ADDED_DATA_SET; i++) array.add(i);
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("Adding to End:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + array.size() + "\n");
+    }
+
+    public void addToMiddleArrayList() {
+        ArrayList array = new ArrayList();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < SIZE_OF_ADDED_DATA_SET; i++) {
+            int middleIndex = array.size() / 2;
+            array.add(i, middleIndex);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("Adding to Middle:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + array.size() + "\n");
+    }
+
+    public void addToFrontLinkedList() {
+        LinkedList linked = new LinkedList();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < SIZE_OF_ADDED_DATA_SET; i++) {
+            linked.add(i, 0);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("Adding to Front:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + linked.size() + "\n");
+    }
+
+    public void addToEndLinkedList() {
+        LinkedList linked = new LinkedList();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < SIZE_OF_ADDED_DATA_SET; i++) {
+            linked.add(i);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("Adding to End:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + linked.size() + "\n");
+    }
+
+    public void addToMiddleLinkedList() {
+        LinkedList linked = new LinkedList();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < SIZE_OF_ADDED_DATA_SET; i++) {
+            int middleIndex = linked.size() / 2;
+            linked.add(i, middleIndex);
+        }
+        endTime = System.nanoTime();
+        duration = endTime - startTime;
+
+        System.out.println("Adding to Middle:");
+        System.out.println("Time: " + (duration / 1000000.0) + " milliseconds");
+        System.out.println("Final size: " + linked.size() + "\n");
+    }
+}
